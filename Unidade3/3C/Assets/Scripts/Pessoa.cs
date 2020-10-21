f4ddeb76ffe0f635dde8ce41fcd6f1875fd1afaf
Gian Carlo <giancarlogg16@gmail.com>
2020-10-20 21:01:31 -0300

    3H

---
+++ Unidade3/3C/Assets/Scripts/Pessoa.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pessoa : MonoBehaviour
+{
+    public long Id { get; set; }
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+}
---
+++ Unidade3/3C/Assets/Scripts/Usuario.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Usuario : MonoBehaviour
+{
+    public long Id  { get; set; }
+    public string Email { get; set; }
+    public string Senha { get; set; }
+}
